b06e537c7dcfd7a8f8375c8dd6ec2c7034f03d35
Leroy Vaughan <leroy@glvconsulting.com>
2014-02-15 17:49:04 -0800

    Responsive CSS Demo
    A single page demonstration of Responsive CSS without Bootstrap.
    Uses:
    - Respond.js
    - CSS3 Pie
    - JQuery
    - some native javascript

---
+++ js/main.js
@@ -0,0 +1,60 @@
+﻿
+$(function () {
+    var viewElem = $("#cssViewPort");
+    var viewPort = viewElem.css("left");
+    var isActiveBtn = false;
+
+    var makeActiveBtn = function (makeActive) {
+
+        if (makeActive && !isActiveBtn) {
+            isActiveBtn = true;
+            var _nav = $("#navWrap");
+
+            $("#menuBtn").click(function () {
+                _nav.show();
+
+                _nav.hover(function () {
+                    //no code for hover
+                    },
+                    function(){
+                        $(this).hide();
+                        $(this).off("hover");
+                });
+            });
+        }
+
+        if (!makeActive && isActiveBtn) {
+            isActiveBtn = false;
+            $("#menuBtn").off("click");
+        }
+    };
+
+
+    var getViewPortSize = function() {
+        viewPort = viewElem.css("left");
+        viewPort = parseInt(viewPort.substring(0, viewPort.indexOf("px"))) * -1;
+
+        if (viewPort <= 910) {
+            makeActiveBtn(true);
+        }
+        else {
+            makeActiveBtn(false);
+        }
+
+        //console.log(viewPort);
+
+        return this;
+    };
+    getViewPortSize();
+
+
+
+    $(window).resize(function () {
+        /*
+            get the left absolute position of #cssViewPort as that
+            is the maxWidth of the screen
+        */
+        getViewPortSize();
+    });
+
+});
